8b3e7e35914c7923b6943a7ed763d4c2a802dbdf
tairakbayev@gmail.com <tair@MacBook-Air-Tair.local>
2024-12-16 16:37:09 +0500

    master Student.java added

---
+++ src/OOPastanaitu/lecture1online/Student.java
@@ -0,0 +1,26 @@
+public class Student {
+    public Student() {
+
+    }
+
+    public static int var = 1234;
+    protected int barcode;
+    private int gpa;
+
+    public int getGPA() {
+        return this.gpa;
+    }
+
+    public void setGPA(int b) {
+        this.gpa = b;
+    }
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
